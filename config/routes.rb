4e20b3df7de868faa74d4acdadc1b4a8e00f7542
toobaiqbal <tooba.iqbal986@gmail.com>
2019-09-16 15:00:06 +0500

    Complete Application

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  resources :todo_lists do
+  resources :todo_items do
+
+    member do
+      patch :complete
+
+  end
+
+    end
+  end
+
+root "todo_lists#index"
+
+
+end
